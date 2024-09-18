05a194ca66f3e60540847397eacc6e27a30bc15b
VBuzorina <vitalina.buzorina@gmail.com>
2024-09-18 17:35:40 +0300

    step 2 — refactoring

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,13 +6,15 @@ import genDiff from '../src/index.js';
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0')
+  .version('0.0.1')
   .option('-f, --format [type]', 'output format', 'stylish')
   .argument('<filepath1>')
   .argument('<filepath2>')
-  .helpOption('-h, --help', 'display help for command')
-  .action((filepath1, filepath2, options) => {
-    console.log(genDiff(filepath1, filepath2, options.format));
+  .action((filepath1, filepath2) => {
+    const formatter = program.opts().format;
+
+    const diff = genDiff(filepath1, filepath2, formatter);
+    console.log(diff);
   });
 
 program.parse();
